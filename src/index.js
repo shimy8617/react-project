602b48ff2b03db34253d101215adfa104e9f91c6
Cristina Shim <84110240+shimy8617@users.noreply.github.com>
2022-03-31 13:34:24 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -38,7 +38,9 @@ const Book = ({img, title, author}) => {
 
     //attribute, eventHandler
     //onClick, onMouseOver
-    const clickHandler = () => {
+    const clickHandler = (e) => {
+        console.log(e);
+        console.log(e.target);
         alert('hello world');
     }
     const complexExample = (author) => {
@@ -46,7 +48,10 @@ const Book = ({img, title, author}) => {
     }
 
     return (
-        <article className='book'>
+        <article className='book' onMouseOver={() => {
+            console.log(title);
+
+        }}>
             <img src={img} alt='' />
             <h1 onClick={()=> console.log(title)}>{title}</h1>
             <h4>{author}</h4>
