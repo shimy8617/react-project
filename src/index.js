a4b364296e08837377355e371302c9cc103d9cc4
Cristina Shim <84110240+shimy8617@users.noreply.github.com>
2022-03-31 13:19:16 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -6,38 +6,54 @@ import ReactDOM from 'react-dom';
 
 const books = [
     {
+        id: 1,
         img: 'https://m.media-amazon.com/images/I/81KvOPlBd0L._AC_UL320_.jpg',
         title: 'Sold on a Monday: A Novel',
         author: 'Kristina McMorris',
     },
     {
+        id: 2,
         img: 'https://m.media-amazon.com/images/I/61pZUq-dKyL._AC_UL320_.jpg',
         title: 'Watching You: A Novel',
         author: 'Lisa Jewell',
     },
+    {
+        id: 3,
+        img: 'https://m.media-amazon.com/images/I/81YK2vnrqFL._AC_UL320_.jpg',
+        title: 'Devoted',
+        author: 'Dean Koontz',
+    }, 
 ];
 
 function BookList() {
     return <section className='booklist'>
-        {books.map((book) => {
-        const {img, title, author} = book;
-        return (
-        <div>
-            <h3>{title}</h3>
-            <h6>{author}</h6>
-        </div>
-        );
+        {books.map((book,index) => {
+        return <Book key={book.id} {...book}></Book>;
     })}
     </section>
 }
-const Book = (props) => {
-    const { img, title, author } = props;
+const Book = ({img, title, author}) => {
+    /*console.log(props);
+    const { img, title, author } = props;*/
+
+    //attribute, eventHandler
+    //onClick, onMouseOver
+    const clickHandler = () => {
+        alert('hello world');
+    }
+    const complexExample = (author) => {
+        console.log(author);
+    }
 
     return (
         <article className='book'>
             <img src={img} alt='' />
-            <h1>{title}</h1>
+            <h1 onClick={()=> console.log(title)}>{title}</h1>
             <h4>{author}</h4>
+            <button type="button" onClick={clickHandler}>
+                reference example
+            </button>
+            <button type="button" onClick={() => complexExample(author)}>more complex example</button>
         </article>
     )
 }
